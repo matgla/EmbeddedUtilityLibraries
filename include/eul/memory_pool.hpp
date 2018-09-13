f9c07570ef9b95bc0a95fb01127e126448f74090
Mateusz Stadnik <matgla@live.com>
2018-09-13 18:06:18 +0000

    wip - deallocation

--- include/eul/memory_pool.hpp
+++ include/eul/memory_pool.hpp
@@ -9,56 +9,124 @@
 
 namespace eul
 {
-
-template <std::size_t Size>
-class MemoryPool final
+    
+template<typename Type, typename PoolType>
+struct pool_unique_ptr
 {
-private:
-    struct AllocationInfo
+    using SizeType = typename PoolType::SizeType;
+    pool_unique_ptr(Type* ptr, PoolType& pool, SizeType positionInBuffer) : ptr_(ptr), pool_(pool), positionInBuffer_(positionInBuffer)
     {
-        std::size_t size;
+    }
+    
+    ~pool_unique_ptr()
+    {
+        ptr_->~Type();
+        pool_.deallocate(this);
+    }
+    
+    Type* operator->() const noexcept
+    {
+        return ptr_;
+    }
+    
+    void reset()
+    {
+        if (!ptr_) 
+        { 
+            return;
+        }
+        
+        ptr_->~Type();
+        pool_.deallocate(this);
+        
+        ptr_ = nullptr;
+    }
+    
+    const SizeType position() const
+    {
+        return positionInBuffer_;
+    }
+    
+    Type* ptr_;
+    PoolType& pool_;
+    SizeType positionInBuffer_;
+};
+    
+template<typename Type, typename PoolType>
+auto make_pool_unique_ptr(Type* ptr, PoolType& pool, PoolType::SizeType positionInBuffer)
+{
+    return pool_unique_ptr(ptr, pool, positionInBuffer);
+}
+    
+template<typename SizeType>
+struct AllocationInfo
+{
+    enum class State : uint8_t
+    {
+        Reserved = 0xAC,
+        Free = 0x00
     };
+    
+    State state;
+    SizeType size;
+};
 
-    using AlignedStorageType = typename std::aligned_storage<sizeof(void*), alignof(void*)>::type;
+template <typename TSizeType, TSizeType Size = 128>
+struct MemoryPool
+{
+    using Storage = std::aligned_storage<sizeof(std::max_align_t), alignof(std::max_align_t)>::type;
+    using SizeType = TSizeType;
 
-public:
-    MemoryPool()
-        : firstObjectAddress_(nullptr)
+    MemoryPool() : start_(0), current_(0), end_(0) 
     {
+    }
+    
+    template <typename T>
+    constexpr static T ceilingDivision(T a, T b)
+    {
+        return a / b + (a % b != 0);
+    }
+    
+    constexpr static int getAllocationHeaderSize()
+    {
+        return ceilingDivision(sizeof(AllocationInfo<SizeType>), sizeof(std::max_align_t));
     }
 
     template <typename T, typename... Args>
-    T* allocate(Args... args)
+    auto allocate(Args&&... args)
     {
-        auto* positionInMemory = getNext();
-        // new (positionInMemory) AllocationInfo{sizeof(T)};
-        // auto* object = new (positionInMemory + sizeof(AllocationInfo)) T(std::forward<Args>(args)...);
+        constexpr auto allocationSize = ceilingDivision(sizeof(T), sizeof(std::max_align_t)) + getAllocationHeaderSize();
+        static_assert(allocationSize <= Size, "Not enough space in buffer");
+        const auto freeSpace = (data_.size() - current_);
 
-        for (const auto& data : data_)
+        std::cerr << "allocate: " << std::to_string(allocationSize) << ", free: " << std::to_string(freeSpace) << std::endl; 
+        if (allocationSize > freeSpace)
         {
-            std::cerr << std::hex << "0x";
-            for (const auto byte : data.__data)
-            {
-                std::cerr << std::to_string(byte);
-            }
-            std::cerr << " ";
+            return nullptr;
         }
-
-        std::cerr << std::endl;
-        return nullptr;
+        current_ = end_;
+        AllocationInfo<SizeType>* allocationHeader = new(&data_[current_]) AllocationInfo<SizeType>;
+        allocationHeader->state = AllocationInfo<SizeType>::State::Reserved;
+        allocationHeader->size = sizeof(T) / sizeof(std::max_align_t) + (sizeof(T) % sizeof(std::max_align_t) != 0);
+        SizeType headerPositionInBuffer = current_;
+        current_ = end_;
+        current_ += getAllocationHeaderSize();
+        end_ = current_ + allocationHeader->size;
+        auto* object = new (&data_[current_]) T(std::forward<Args>(args)...);
+        return make_pool_unique_ptr(object, *this, headerPositionInBuffer);
     }
-
-private:
-    AlignedStorageType* getNext()
+    
+    template <typename T>
+    void deallocate(pool_unique_ptr<T, decltype(*this)>* data)
     {
-        if (firstObjectAddress_ == nullptr)
-        {
-            firstObjectAddress_ = data_.data();
-        }
+        AllocationInfo<SizeType>* header = reinterpret_cast<AllocationInfo<SizeType>*>(data_[data.position()]);
+        std::cerr << "0x: " << std::hex << static_cast<int>(header->state) << std::endl;
     }
-
-    AlignedStorageType* firstObjectAddress_;
-    std::array<AlignedStorageType, Size> data_;
+    
+    int start_;
+    int current_;
+    int end_;
+    std::array<Storage, Size/sizeof(std::max_align_t)> data_;
 };
 
 } // namespace eul
--- test/UT/MemoryPoolTests.cpp
+++ test/UT/MemoryPoolTests.cpp
@@ -19,12 +19,130 @@ private:
     bool constructed_ = false;
 };
 
+struct AllocationTestObject
+{
+    int a;
+    double b;
+    char c;
+    long double d;
+};
+
+class DestructorTester
+{
+public:
+    DestructorTester(bool& destructed) : destructed_(destructed)
+    {
+        destructed = false;
+    }
+    
+    ~DestructorTester()
+    {
+        destructed_ = true;
+    }
+    
+private:
+    bool& destructed_;
+};
+
 TEST_CASE("MemoryPool should", "[MemoryPool]")
 {
     SECTION("call constructor")
     {
-        eul::MemoryPool<128> sut;
-        auto* test = sut.allocate<ConstructorTester>();
-        // REQUIRE(test->constructed());
+        eul::MemoryPool<uint8_t, 128> sut;
+        auto test = sut.allocate<ConstructorTester>();
+        REQUIRE(test->constructed());
+    }
+    
+    SECTION("allocate data")
+    {
+        eul::MemoryPool<uint8_t, 128> sut;
+        auto test = sut.allocate<AllocationTestObject>();
+        REQUIRE(test != nullptr);
+        constexpr int integer1 = 123;
+        constexpr double double1 = -223.123;
+        constexpr char char1 = 'a';
+        constexpr long double ldouble1 = 1231231234412.123123123;
+        constexpr int integer2 = -12344;
+        constexpr double double2 = 123512.1235325;
+        constexpr char char2 = 'c';
+        constexpr long double ldouble2 = -1234566788.865432;
+        
+        test->a = integer1;
+        test->b = double1;
+        test->c = char1;
+        test->d = ldouble1;
+        REQUIRE(test->a == integer1);
+        REQUIRE(test->b == double1);
+        REQUIRE(test->c == char1);
+        REQUIRE(test->d == ldouble1);
+        
+        auto test2 = sut.allocate<AllocationTestObject>();
+        REQUIRE(test2 != nullptr);
+        test2->a = integer2;
+        test2->b = double2;
+        test2->c = char2;
+        test2->d = ldouble2;
+        REQUIRE(test->a == integer1);
+        REQUIRE(test->b == double1);
+        REQUIRE(test->c == char1);
+        REQUIRE(test->d == ldouble1);
+        
+        REQUIRE(test2->a == integer2);
+        REQUIRE(test2->b == double2);
+        REQUIRE(test2->c == char2);
+        REQUIRE(test2->d == ldouble2);
+
+        sut.deallocate(test);
+        REQUIRE(test2->a == integer2);
+        REQUIRE(test2->b == double2);
+        REQUIRE(test2->c == char2);
+        REQUIRE(test2->d == ldouble2);
+
+    }
+    
+    SECTION("don't overflow buffer")
+    {
+        eul::MemoryPool<uint8_t, sizeof(AllocationTestObject)/sizeof(uint8_t)*2> sut;
+        auto test = sut.allocate<AllocationTestObject>();
+        REQUIRE(test != nullptr);
+        constexpr int integer1 = 123;
+        constexpr double double1 = -223.123;
+        constexpr char char1 = 'a';
+        constexpr long double ldouble1 = 1231231234412.123123123;
+        constexpr int integer2 = -12344;
+        constexpr double double2 = 123512.1235325;
+        constexpr char char2 = 'c';
+        constexpr long double ldouble2 = -1234566788.865432;
+        
+        test->a = integer1;
+        test->b = double1;
+        test->c = char1;
+        test->d = ldouble1;
+        REQUIRE(test->a == integer1);
+        REQUIRE(test->b == double1);
+        REQUIRE(test->c == char1);
+        REQUIRE(test->d == ldouble1);
+        
+        auto test2 = sut.allocate<AllocationTestObject>();
+        REQUIRE(test2 == nullptr);
+        REQUIRE(test->a == integer1);
+        REQUIRE(test->b == double1);
+        REQUIRE(test->c == char1);
+        REQUIRE(test->d == ldouble1);
+    }
+    
+    SECTION("Destroys object while deallocation")
+    {
+        eul::MemoryPool<uint8_t, 128> sut;
+        bool isObject1Destroyed = false;
+        bool isObject2Destroyed = false;
+        {
+            auto obj1 = sut.allocate<DestructorTester>(isObject1Destroyed);
+            auto obj2 = sut.allocate<DestructorTester>(isObject2Destroyed);
+            sut.deallocate(obj1);
+            sut.deallocate(obj2);
+        }
+        REQUIRE(isObject1Destroyed);
+        REQUIRE(isObject2Destroyed);
     }
 }
