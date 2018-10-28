a2edd60203c3ee38899ed88fb2da2f392ee04aaf
Mateusz Stadnik <matgla@live.com>
2018-10-28 23:14:45 +0100

    trait for pointer size

--- include/eul/traits.hpp
+++ include/eul/traits.hpp
@@ -10,7 +10,14 @@ template <typename Tuple, typename Type>
 struct has_type;
 
 template <typename Type, typename... Args>
-struct has_type<std::tuple<Args...>, Type> : std::disjunction<std::is_same<Type, Args>...>
+struct has_type<std::tuple<Args...>, Type>
+    : std::disjunction<std::is_same<Type, Args>...>
 {
 };
+
+struct pointer_size
+{
+    constexpr static std::size_t size = sizeof(void*);
+};
+
 } // namespace eul
