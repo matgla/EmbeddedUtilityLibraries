26c1567a8452aee130155d0db0393c90d835b1ba
Mateusz Stadnik <matgla@live.com>
2019-05-14 21:22:49 +0200

    added fwd for kernel

---
+++ include/eul/kernel/fwd.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace eul
+{
+namespace kernel
+{
+
+template <typename Event>
+struct EventListener;
+
+class Kernel;
+
+class Module;
+
+} // namespace kernel
+} // namespace eul
