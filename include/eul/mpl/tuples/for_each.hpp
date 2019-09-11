7de9541dc3d37b0bec9c48803dfed01dadf8ac02
Mateusz Stadnik <matgla@live.com>
2019-09-11 17:17:56 +0200

    fixes in module management

--- include/eul/mpl/tuples/for_each.hpp
+++ include/eul/mpl/tuples/for_each.hpp
@@ -12,18 +12,18 @@ namespace tuples
 namespace detail
 {
 
-template <typename Tuple, std::size_t Index>
-constexpr void for_each_impl(const auto& fn, Tuple& tuple)
+template <typename Tuple, std::size_t Index, typename Fn>
+constexpr void for_each_impl(const Fn& fn, Tuple& tuple)
 {
     fn(std::get<Index>(tuple));
     if constexpr (Index + 1 < std::tuple_size<Tuple>::value)
     {
-        for_each_impl<Tuple, Index + 1>(fn, tuple);
+        for_each_impl<Tuple, Index + 1, Fn>(fn, tuple);
     }
 }
 
-template <template <typename> typename Pred, typename Tuple, std::size_t Index>
-constexpr void for_each_impl(const auto& fn, Tuple& tuple)
+template <template <typename> typename Pred, typename Tuple, std::size_t Index, typename Fn>
+constexpr void for_each_impl(const Fn& fn, Tuple& tuple)
 {
     if constexpr (Pred<typename std::tuple_element<Index, Tuple>::type>::value)
     {
@@ -32,22 +32,22 @@ constexpr void for_each_impl(const auto& fn, Tuple& tuple)
 
     if constexpr (Index + 1 < std::tuple_size<Tuple>::value)
     {
-        for_each_impl<Pred, Tuple, Index + 1>(fn, tuple);
+        for_each_impl<Pred, Tuple, Index + 1, Fn>(fn, tuple);
     }
 }
 
 } // namespace detail
 
-template <typename Tuple, std::size_t Size = 0>
-constexpr void for_each(Tuple& t, const auto& fn)
+template <typename Tuple, typename Fn, std::size_t Size = 0>
+constexpr void for_each(Tuple& t, const Fn& fn)
 {
-    detail::for_each_impl<Tuple, 0>(fn, t);
+    detail::for_each_impl<Tuple, 0, Fn>(fn, t);
 }
 
-template <template <typename> typename Pred, typename Tuple>
-constexpr void for_each(Tuple& t, const auto& fn)
+template <template <typename> typename Pred, typename Tuple, typename Fn>
+constexpr void for_each(Tuple& t, const Fn& fn)
 {
-    detail::for_each_impl<Pred, Tuple, 0>(fn, t);
+    detail::for_each_impl<Pred, Tuple, Fn, 0>(fn, t);
 }
 
 } // namespace tuples
