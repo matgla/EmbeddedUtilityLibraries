9d3a98bdd5e1972fa013e01e2839055e24b1b874
Mateusz Stadnik <matgla@live.com>
2020-01-27 19:16:54 +0100

    added noncopyable

---
+++ include/eul/utils/noncopyable.hpp
@@ -0,0 +1,34 @@
+// This file is part of EUL project. 
+// Copyright (C) 2020 Mateusz Stadnik
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once 
+
+namespace eul
+{
+namespace utils
+{
+
+class noncopyable
+{
+public:
+    noncopyable() = default;
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+};
+
+} // namespace utils     
+} // namespace eul 
+
